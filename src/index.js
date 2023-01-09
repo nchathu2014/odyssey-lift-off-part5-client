c8d1c36066b80e927f9d08490c662982686194b2
nchathu2014 <nuwan.thuduwage@gmail.com>
2023-01-09 09:22:21 -0500

    Changed uri

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  //uri: "https://odyssey-lift-off-part5-server-production-5fa4.up.railway.app/", // change to YOUR own production server
-  uri:"http://localhost:4000",
+  uri: "https://odyssey-lift-off-part5-server-production-5fa4.up.railway.app/", // change to YOUR own production server
+  //uri:"http://localhost:4000",
   cache: new InMemoryCache(),
 });
 
